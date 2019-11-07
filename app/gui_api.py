74738a5347a4d5a45bb89e0dbca96c8767487d7b
scottctaylor12 <scott.c.taylor12@gmail.com>
2019-11-07 13:52:15 -0500

    edits to work with recent change in caldera bleeding edge code

--- app/gui_api.py
+++ app/gui_api.py
@@ -1,5 +1,7 @@
-from aiohttp_jinja2 import template
+import logging
+import traceback
 
+from aiohttp_jinja2 import template
 
 class EvalsApi:
 
@@ -7,6 +9,7 @@ class EvalsApi:
         self.auth_svc = services.get('auth_svc')
         self.plugin_svc = services.get('plugin_svc')
         self.data_svc = services.get('data_svc')
+        self.app_svc = services.get('app_svc')
 
     @staticmethod
     def sort_name(name):
@@ -15,14 +18,16 @@ class EvalsApi:
     @template('evals.html')
     async def splash(self, request):
         await self.auth_svc.check_permissions(request)
-        plugins = [dict(name=getattr(p, 'name'), address=getattr(p, 'address')) for p in self.plugin_svc.get_plugins()]
+        plugins = [dict(name=getattr(p, 'name'), address=getattr(p, 'address')) for p in self.app_svc.get_plugins()]
 
         eval_full = 'ef93dd1b-809b-4a0b-b686-fef549cabbe4'
-        adversary = (await self.data_svc.explode_adversaries(criteria=dict(adversary_id=eval_full)))[0]
+        # adversary = (await self.data_svc.explode_adversaries(criteria=dict(adversary_id=eval_full)))[0]
+        adversary = (await self.data_svc.locate('adversaries', dict(adversary_id=eval_full)))[0]
+        logging.debug(type(adversary))
         all_steps = []
-        for phase, steps in adversary['phases'].items():
+        for phase, steps in adversary.phases.items():
             all_steps.extend(steps)
-        all_steps = list({v['name']: v for v in all_steps}.values())
-        all_steps = sorted(all_steps, key=lambda i: self.sort_name(i['name']))
+        all_steps = list({v.name: v for v in all_steps}.values())
+        all_steps = sorted(all_steps, key=lambda i: self.sort_name(i.name))
 
         return dict(plugins=plugins, adversary=adversary, steps=all_steps)
